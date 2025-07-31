7f8f7581b410ebfd0d208029cf9c51d9816b8104
Nikita Berezhnyj <97048105+NikitaBerezhnyj@users.noreply.github.com>
2025-07-31 13:53:49 +0300

    feat: updated assets loading algorithm in RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,18 @@
+import { useLoadAssets } from "@/hooks/useLoadAssets";
+import { useTheme } from "@react-navigation/native";
 import { Stack } from "expo-router";
+import { View } from "react-native";
 import "react-native-reanimated";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
+  const ready = useLoadAssets();
+  const { colors } = useTheme();
+
+  if (!ready) {
+    return <View style={{ flex: 1, backgroundColor: colors.background }} />;
+  }
+
   return (
     <SafeAreaProvider>
       <Stack screenOptions={{ headerShown: false }}>
