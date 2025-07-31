5ea8a8d28c9391005f98132b4cf34c4bf81505a7
Nikita Berezhnyj <97048105+NikitaBerezhnyj@users.noreply.github.com>
2025-07-31 21:52:08 +0300

    chore: setup project envirement for testing

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   preset: "jest-expo",
   setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
   transformIgnorePatterns: [
-    "node_modules/(?!(@react-native|react-native|expo(nent)?|@expo(nent)?)/)"
+    "node_modules/(?!(@react-native|react-native|expo-font|expo-asset|expo-constants|expo-splash-screen|expo(nent)?|@expo(nent)?|expo-modules-core)/)"
   ]
 };
